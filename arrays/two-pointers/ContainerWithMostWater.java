f87a7b1ce5a282b566054682750218fcc1a97022
Shrexeideas <s85481536@gmail.com>
2026-02-12 23:48:22 +0530

    Add maxArea method to calculate container water
    Implement brute-force solution to calculate maximum water area.

--- arrays/two-pointers/ContainerWithMostWater.java
+++ arrays/two-pointers/ContainerWithMostWater.java
@@ -1,1 +1,17 @@
+class Solution {
+    public int maxArea(int[] height) {
+        
+  int n = height.length ;
+int maxWater = 0;
+for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
 
+                int minHeight= Math.min(height[i],height[j]);
+                int width =j -i;
+                int area = minHeight * width;
+                maxWater = Math.max(maxWater,area);
+            }
+        }
+        return maxWater;
+    }
+}
